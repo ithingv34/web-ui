ba65cab7b31ebd319b476e681b442a052e1c8475
ithingv34 <ithingv34@gmail.com>
2023-06-04 15:55:11 +0900

    TempInfo 컴포넌트 구현, 체감온도, 최고,최저온도

--- weather-app/src/WeatherApp.jsx
+++ weather-app/src/WeatherApp.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './style.css'
 import CurrentWeather from './CurrentWeather/CurrentWeather'
+import TempInfo from './TempInfo/TempInfo'
 
 function WeatherApp() {
   return (
     <>
     <div className='container'>
         <CurrentWeather/>
+        <TempInfo/>
     </div>
     </>
   )
