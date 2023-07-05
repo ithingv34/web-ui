e93e54b4292aaea807d3b156cf012e2994116357
ithingv34 <ithingv34@gmail.com>
2023-07-05 20:31:49 +0900

    configure react-query and first fetch data

--- data-fetch/src/App.js
+++ data-fetch/src/App.js
@@ -1,7 +1,31 @@
+import { useQuery } from "react-query";
+
+const fetchRepositories = async () => {
+  const response = await fetch(
+    "https://api.github.com/search/repositories?q=topic:reactjs&per_page=30&page=1"
+  ).then((res) => res.json());
+  return response;
+};
+
 function App() {
+  const { isLoading, error, data } = useQuery(
+    "repositories",
+    fetchRepositories
+  );
+
   return (
     <main>
-      <h1>Infinite Scroll</h1>
+      <h1>infinite scroll</h1>
+      <ul>
+        {data.items.map((repo) => (
+          <li key={repo.id}>
+            <p>
+              <b>{repo.name}</b>
+            </p>
+            <p>{repo.description}</p>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
