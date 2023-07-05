d573a14b0164b379a62b672f8abbe99ee062e34b
ithingv34 <ithingv34@gmail.com>
2023-07-05 21:36:55 +0900

    add infinite query

--- data-fetch/src/App.js
+++ data-fetch/src/App.js
@@ -1,33 +1,71 @@
-import { useQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
+import { useEffect, useState } from "react";
+import { Spinner } from "@material-tailwind/react";
 
-const fetchRepositories = async () => {
+const fetchRepositories = async (page = 1) => {
   const response = await fetch(
-    "https://api.github.com/search/repositories?q=topic:reactjs&per_page=30&page=1"
-  ).then((res) => res.json());
-  return response;
+    `https://api.github.com/search/repositories?q=topic:reactjs&per_page=30&page=${page}`
+  );
+  return response.json();
 };
 
-function App() {
-  const { isLoading, error, data } = useQuery(
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const { data, hasNextPage, fetchNextPage } = useInfiniteQuery(
     "repositories",
-    fetchRepositories
+    ({ pageParam = 1 }) => fetchRepositories(pageParam),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        const maxPages = lastPage.total_count / 30;
+        const nextPage = allPages.length + 1;
+        return nextPage <= maxPages ? nextPage : undefined;
+      },
+    }
   );
+
+  useEffect(() => {
+    let fetching = false;
+    const onScroll = async (event) => {
+      const { scrollHeight, scrollTop, clientHeight } =
+        event.target.scrollingElement;
+
+      if (!fetching && scrollHeight - scrollTop <= clientHeight * 1.5) {
+        fetching = true;
+        setLoading(true);
+        if (hasNextPage) {
+          await fetchNextPage();
+        }
+        setLoading(false);
+        fetching = false;
+      }
+    };
+
+    document.addEventListener("scroll", onScroll);
+    return () => {
+      document.removeEventListener("scroll", onScroll);
+    };
+  }, [hasNextPage, fetchNextPage]);
+
+  console.log(data);
 
   return (
     <main>
-      <h1>infinite scroll</h1>
+      <h1>Infinite Scroll</h1>
       <ul>
-        {data.items.map((repo) => (
-          <li key={repo.id}>
-            <p>
-              <b>{repo.name}</b>
-            </p>
-            <p>{repo.description}</p>
-          </li>
-        ))}
+        {data.pages.map((page) =>
+          page.items.map((repo) => (
+            <li key={repo.id}>
+              <p>
+                <b>{repo.name}</b>
+              </p>
+              <p>{repo.description}</p>
+            </li>
+          ))
+        )}
       </ul>
+      {loading ? <Spinner color="blue" /> : <p>{loading}</p>}
     </main>
   );
-}
+};
 
 export default App;
